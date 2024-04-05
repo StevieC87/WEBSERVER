94b05b2be3d49e2ba8cb960e1838d80517f25410
StevieC87 <92942489+StevieC87@users.noreply.github.com>
2024-04-05 23:12:19 +0200

    jlkkjncvb

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Define custom Express routes here, e.g.:
+  // server.get('/custom-route', (req, res) => res.send('Hello World!'));
+
+  // Next.js page routing:
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/">
+        <span>Home</span>
+      </Link>
+      <div>HOMEPAGE</div>
+    </div>
+  );
+};
+
+export default About;
